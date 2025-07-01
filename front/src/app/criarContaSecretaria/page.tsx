d1b8d65bacfe8617c8d01102d808ab71c9095d2a
Erimilson Silva <eesr@discente.ifpe.edu.br>
2025-07-01 20:59:26 -0300

    Remove PrimeiroAcesso component and associated CreateSecretariatAccount import

--- front/src/app/criarContaSecretaria/page.tsx
+++
@@ -1,9 +0,0 @@
-import { CreateSecretariatAccount } from '@/components/CreateSecretariatAccount';
-
-export default function PrimeiroAcesso() {
-  return (
-    <div className="min-h-screen flex w-full">
-      <CreateSecretariatAccount />
-    </div>
-  );
-}
