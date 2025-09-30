a25c5db1ea085a3fb788e57984b77b4b9cd74ac4
Mavi-Tiburcio <mvot@discente.ifpe.edu.br>
2025-09-30 11:19:05 -0300

    fix: corrige formatação e export em loginSchema

--- front/src/components/CardLogin/schemas/schema.ts
+++ front/src/components/CardLogin/schemas/schema.ts
@@ -1,13 +1,8 @@
 import { z } from 'zod';
 
 export const loginSchema = z.object({
-  email: z
-    .string()
-    .nonempty('Campo obrigatório')
-    .email('Email Inválido'),
-  password: z
-    .string()
-    .nonempty('Campo obrigatório')
+  email: z.string().nonempty('Campo obrigatório').email('Email Inválido'),
+  password: z.string().nonempty('Campo obrigatório')
 });
 
 export type LoginSchemaType = z.infer<typeof loginSchema>;
