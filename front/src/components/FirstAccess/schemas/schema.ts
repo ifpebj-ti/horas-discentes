af9ff5c7ec8ad2fbe7ef75d7b847e6b455c28703
Marcelo Chagas <144177832+MarceloDChagas@users.noreply.github.com>
2026-04-04 14:25:52 -0300

    fix: validate "matricula"

--- front/src/components/FirstAccess/schemas/schema.ts
+++ front/src/components/FirstAccess/schemas/schema.ts
@@ -9,7 +9,10 @@ export const firstAccessSchema = z
       .refine((email) => email.endsWith('@discente.ifpe.edu.br'), {
         message: 'Use seu email institucional (@discente.ifpe.edu.br)'
       }),
-    matricula: z.string().min(1, 'Matrícula obrigatória'),
+    matricula: z
+      .string()
+      .length(13, 'A matrícula deve ter exatamente 13 caracteres')
+      .regex(/^.{4}[12].{4}\d{4}$/, 'Matrícula inválida. Exemplo: 20231ewbj2157 (5º dígito: 1 ou 2, últimos 4: números)'),
     senha: z.string().min(8, 'A senha deve ter no mínimo 8 caracteres'),
     confirmarSenha: z.string().min(1, 'Confirme sua senha')
   })
