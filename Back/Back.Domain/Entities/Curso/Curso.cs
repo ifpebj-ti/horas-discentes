26bbc6a78ebd02fd7d812c9462afb76c474a3c68
Marcelo Chagas <144177832+MarceloDChagas@users.noreply.github.com>
2026-04-18 23:09:18 -0300

    feat: add nullable CampusId to Curso entity and WithCampusId to CursoBuilder

--- Back/Back.Domain/Entities/Curso/Curso.cs
+++ Back/Back.Domain/Entities/Curso/Curso.cs
@@ -10,6 +10,9 @@ public class Curso
     [Required(ErrorMessage = "O campo Nome do curso é obrigatório.")]
     public string? Nome { get; set; }
 
+    public Guid? CampusId { get; set; }
+    public Campus.Campus? Campus { get; set; }
+
     public ICollection<Turma.Turma> Turmas { get; set; } = new List<Turma.Turma>();
 
     public ICollection<Coordenador.Coordenador> Coordenadores { get; set; } = new List<Coordenador.Coordenador>();
