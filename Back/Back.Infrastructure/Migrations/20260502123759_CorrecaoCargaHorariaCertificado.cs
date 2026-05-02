e1adee85b89b0adc3aea1b3133fb7fb3d89da802
Marcelo Chagas <144177832+MarceloDChagas@users.noreply.github.com>
2026-05-02 09:38:10 -0300

    feat: add migration for CargaHorariaOriginal and CargaHorariaCorrigida columns

---
+++ Back/Back.Infrastructure/Migrations/20260502123759_CorrecaoCargaHorariaCertificado.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Back.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class CorrecaoCargaHorariaCertificado : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "CargaHorariaCorrigida",
+                table: "Certificados",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<int>(
+                name: "CargaHorariaOriginal",
+                table: "Certificados",
+                type: "integer",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CargaHorariaCorrigida",
+                table: "Certificados");
+
+            migrationBuilder.DropColumn(
+                name: "CargaHorariaOriginal",
+                table: "Certificados");
+        }
+    }
+}
